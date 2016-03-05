9db8434da52c3dc497ef8012d95e02d3fd3afb8e
Saad Khan <saadakhan@utexas.edu>
2016-03-04 20:25:02 -0600

    Added the Tail/Total Histogram

--- PlotMacro.C
+++ PlotMacro.C
@@ -45,38 +45,18 @@ PlotMacro(){
 
   c1->SaveAs("Cold_PEpeaks.jpg");
 
-  TFile *_file0 = TFile::Open("VUV_SiPM_1PE_57V_LED2920_4d3ns_500k.root");
+  TT->SetLineColor(1);
+  TT->GetXaxis()->SetTitle("");
+  TT->GetYaxis()->SetTitle("Entries");
+  TT->GetXaxis()->CenterTitle();
+  TT->GetYaxis()->CenterTitle();
+  TT->SetTitleSize(0.05,"XY");
+  TT->SetTitleOffset(.9, "XY");
+  TT->SetStats(0);
+  TT->Draw();
 
-  hAllWaveforms->GetXaxis()->SetTitle("Time [ns]");
-  hAllWaveforms->GetYaxis()->SetTitle("Voltage [V]");
-  hAllWaveforms->GetXaxis()->CenterTitle();
-  hAllWaveforms->GetYaxis()->CenterTitle();
-  hAllWaveforms->SetTitleSize(0.05,"XY");
-  hAllWaveforms->SetTitleOffset(.9, "XY");
+  legend->Draw("same");
 
-  hAllWaveforms->Draw("colz");
-
-  TLegend* legend2 = new TLegend(0.11,0.91,0.51,0.97,"Room Temperature V_{B}=57V","NDC");
-  legend2->SetTextSize(.05);
-  legend2->SetTextColor(2);
-  legend2->SetFillColor(0);
-  legend2->SetBorderSize(0);
-  legend2->Draw("same");
-
-  c1->SaveAs("Hot_Waveforms.jpg");
-
-  h1->SetLineColor(1);
-  h1->GetXaxis()->SetTitle("Pulse Area [V*ns]");
-  h1->GetYaxis()->SetTitle("Entries");
-  h1->GetXaxis()->CenterTitle();
-  h1->GetYaxis()->CenterTitle();
-  h1->SetTitleSize(0.05,"XY");
-  h1->SetTitleOffset(.9, "XY");
-  h1->SetStats(0);
-  h1->Draw();
-
-  legend2->Draw("same");
-
-  c1->SaveAs("Hot_PEpeaks.jpg");
+  c1->SaveAs(".jpg");
 
 }
